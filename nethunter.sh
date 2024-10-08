6c12d9049dbd6d4ad346a04fae889876be4b0d57
Bhasker Kamshetty <30320555+bhaskerkamshetty@users.noreply.github.com>
2024-10-08 23:15:17 +0530

    Update and rename initial-setup.sh to nethunter.sh

--- nethunter.sh
+++ nethunter.sh
@@ -7,5 +7,14 @@
 
 pkg install root-repo
 pkg install x11-repo
+pkg update -y
+pkg upgrade -y
+pkg install wget unzip zip git nano vim -y
 termux-change-repo
 termux-setup-storage
+wget -O nethunter https://offs.ec/2MceZWr
+chmod +x nethunter
+./nethunter
+nethunter
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install -y kali-linux-default
